802dcfe068226b5ee86c021f8ef068094271eb47
BT <bahartofigh@gmail.com>
2025-03-13 16:27:48 +0330

    get path folder structure

--- popup/popup.js
+++ popup/popup.js
@@ -3,11 +3,12 @@ console.log("extention")
 const btn = document.getElementById("replace_btn");
 
 function replace() {
-    const h1 = document.querySelector("h1");
+    // این آیدی بسیار مهمه
+    // window.location.href = 'chrome-extension://jpkegnchdpngbnfdakamjlcpndnnpjek/popup/dashboard.html'
 
-    h1.innerHTML = "replace extention";
-
-    console.log(h1)
+    //جهت جلوگیری از شلوغی کدها و تکرار نکردن chrome-extension://jpkegnchdpngbnfdakamjlcpndnnpjek
+    let url_file = chrome.runtime.getURL('popup/dashboard.html') //در مسیردهی خیلی مهمه که حواسمون به فولدر باشه
+    chrome.tabs.create({url:url_file})
 }
 
 btn.addEventListener("click" , replace)
