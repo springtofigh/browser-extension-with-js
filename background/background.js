10e307c940e7c3237df578ad51f3339ea581dd65
BT <bahartofigh@gmail.com>
2025-03-24 23:57:34 +0330

    communicate data between plugin files

--- background/background.js
+++ background/background.js
@@ -1,1 +1,11 @@
 console.log("kaftar")
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    //درخواست ارسال شده از فایل پاپ‌آپ
+    // console.log(request.message)
+
+    console.log(sender)
+
+    //از این تابع استفاده میکنیم تا پاسخ به فایل پاپ‌آپ ارسال کنیم
+    sendResponse({ message: "Hello to you" })
+})
