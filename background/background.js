722b898de52769bedb505624618a0110e3fe3442
BT <bahartofigh@gmail.com>
2025-04-08 00:12:17 +0330

    work with local storage

--- background/background.js
+++ background/background.js
@@ -9,3 +9,34 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     //از این تابع استفاده میکنیم تا پاسخ به فایل پاپ‌آپ ارسال کنیم
     sendResponse({ message: "Hello to you" })
 })
+
+
+chrome.storage.local.set({name: "bahar", age: 25}, () => {
+    console.log("data is save")
+})
+
+chrome.storage.local.get(["name", "age"], (result) => {
+    console.log(result)
+    console.log(result.name)
+    console.log(result.age)
+})
+
+// chrome.storage.local.remove(["name"], (result) => {
+//     console.log("The name is remove", result)
+// })
+
+chrome.storage.local.get({ name:"barana" }, (result) => {
+    console.log(result.name)
+})
+
+// chrome.storage.local.remove(["name"], (result) => {
+//     console.log("The name is remove", result)
+// })
+
+// chrome.storage.local.set({age: 27}, () => {
+//     console.log("data is updated")
+// })
+
+// chrome.storage.local.get(["age"], (result) => {
+//     console.log(result.age)
+// })
