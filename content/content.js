0a96f573826b1f12810af8f20e79bc465c097e45
BT <bahartofigh@gmail.com>
2025-05-19 23:39:36 +0330

    get image and assign src

--- content/content.js
+++ content/content.js
@@ -5,13 +5,23 @@
 // })
 
 window.addEventListener("load", () => {
+
+    async function getImg() {
+        const AllImg = document.querySelectorAll("img")
+        const ImgSource = Array.from(AllImg).map(img => img.src)
+        return ImgSource
+    }
+
+
     async function reciveMessage() {
-        chrome.runtime.onConnect.addListener((port) => {
+        chrome.runtime.onConnect.addListener(async (port) => {
             if (port.name === "getImg") {
                 port.onMessage.addListener((msg) => {
-                    console.log(msg);
+                    console.log(msg.message);
                     port.postMessage({ response: "Received your message!" });
                 });
+                const GetImgSource = await getImg()
+                port.postMessage({ response: GetImgSource })
             }
         });
     }
